10d05bb94dce3c7d19a083265d3c8b4ba8df1b8b
Carlos Camacho <carloscfrausto@macbook-pro-de-carlos-5.local>
2016-02-05 12:01:47 -0600

    Initial Commit

---
+++ JuegoMemoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JuegoMemoria
+//
+//  Created by Carlos C.Frausto on 05/02/16.
+//  Copyright © 2016 Carlos C.Frausto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
